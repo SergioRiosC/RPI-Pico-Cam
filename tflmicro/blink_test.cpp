b809f7461ae67db6df548958253bf6510e5e6cf2
sergio <sergiorc2000@hotmail.es>
2026-04-26 16:36:36 -0600

    Add blink_test executable and implementation for LED blinking functionality

---
+++ tflmicro/blink_test.cpp
@@ -0,0 +1,15 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define LED_PIN 22
+
+int main() {
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  while (true) {
+    gpio_put(LED_PIN, 1);
+    sleep_ms(500);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(500);
+  }
+}
