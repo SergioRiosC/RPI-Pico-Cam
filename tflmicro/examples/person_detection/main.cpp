071e079d5ac3ede00294636e4e91f9120970adc5
sergio <sergiorc2000@hotmail.es>
2026-04-26 16:06:08 -0600

    Refactor main function to improve LED initialization and status indication

--- tflmicro/examples/person_detection/main.cpp
+++ tflmicro/examples/person_detection/main.cpp
@@ -1,26 +1,28 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 #include "main_functions.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
-// This is the default main used on systems that have the standard C entry
-// point. Other devices (for example FreeRTOS or ESP32) that have different
-// requirements for entry code (like an app_main function) should specialize
-// this main.cc file in a target-specific subfolder.
+#define LED_PIN 25
+
 int main(int argc, char* argv[]) {
+  // Inicializar LED
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  // 5 parpadeos rápidos al arrancar = programa inició OK
+  for (int i = 0; i < 5; i++) {
+    gpio_put(LED_PIN, 1); sleep_ms(100);
+    gpio_put(LED_PIN, 0); sleep_ms(100);
+  }
+
   setup();
+
+  // 2 parpadeos lentos = setup() completado OK
+  for (int i = 0; i < 2; i++) {
+    gpio_put(LED_PIN, 1); sleep_ms(500);
+    gpio_put(LED_PIN, 0); sleep_ms(500);
+  }
+
   while (true) {
     loop();
   }
