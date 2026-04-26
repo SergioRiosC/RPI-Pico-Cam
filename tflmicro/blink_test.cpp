4d8c7cbd08701ddfeed1fbe47ee8e3e026926faf
sergio <sergiorc2000@hotmail.es>
2026-04-26 17:46:29 -0600

    Enhance blink_test functionality by adding LED_EXT control and updating build verification steps

--- tflmicro/blink_test.cpp
+++ tflmicro/blink_test.cpp
@@ -2,14 +2,19 @@
 #include "hardware/gpio.h"
 
 #define LED_PIN 25
+#define LED_EXT 0
 
 int main() {
   gpio_init(LED_PIN);
+  gpio_init(LED_EXT);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_set_dir(LED_EXT, GPIO_OUT);
   while (true) {
     gpio_put(LED_PIN, 1);
+    gpio_put(LED_EXT, 1);
     sleep_ms(500);
     gpio_put(LED_PIN, 0);
+    gpio_put(LED_EXT, 0);
     sleep_ms(500);
   }
 }
