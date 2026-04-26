67a43ed5b5a6f09977cf4f5f9dfc7b59e55d6b7e
sergio <sergiorc2000@hotmail.es>
2026-04-26 16:23:06 -0600

    Refactor LED initialization and blinking logic in main function

--- tflmicro/examples/person_detection/main.cpp
+++ tflmicro/examples/person_detection/main.cpp
@@ -1,29 +1,16 @@
-#include "main_functions.h"
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 #define LED_PIN 22
 
 int main(int argc, char* argv[]) {
-  // Inicializar LED
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  // 5 parpadeos rápidos al arrancar = programa inició OK
-  for (int i = 0; i < 5; i++) {
-    gpio_put(LED_PIN, 1); sleep_ms(100);
-    gpio_put(LED_PIN, 0); sleep_ms(100);
-  }
-
-  setup();
-
-  // 2 parpadeos lentos = setup() completado OK
-  for (int i = 0; i < 2; i++) {
-    gpio_put(LED_PIN, 1); sleep_ms(500);
-    gpio_put(LED_PIN, 0); sleep_ms(500);
-  }
-
   while (true) {
-    loop();
+    gpio_put(LED_PIN, 1);
+    sleep_ms(500);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(500);
   }
 }
